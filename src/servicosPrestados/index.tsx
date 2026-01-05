d6bdb5bc856d5734be643e0dd535a4d3f7d02de7
Micael Santos <micaelsdsilva@gmail.com>
2026-01-05 17:04:18 -0300

    fix: rewrite routes for SPA

--- src/servicosPrestados/index.tsx
+++ src/servicosPrestados/index.tsx
@@ -2,25 +2,25 @@ export const services = [
   {
     title: "Desenvolvimento Web",
     description:
-      "Criação de sites e aplicações modernas usando React, Tailwind e boas práticas de programação para performance e responsividade.",
-    color: "#2683FF", // neon cyan
+      "Desenvolvimento de sites e aplicações web modernas, focadas em performance, usabilidade e responsividade, utilizando tecnologias como React, Tailwind CSS e boas práticas de programação.",
+    color: "#2683FF", 
   },
   {
     title: "Landing Page",
     description:
-      "Desenvolvimento e manutenção de sistemas de automação, programação de PLCs e integração de máquinas industriais.",
-    color: "#2683FF", // neon purple
+      "Criação de landing pages estratégicas, otimizadas para conversão, com design responsivo, carregamento rápido e foco em resultados para divulgar produtos, serviços ou campanhas.",
+    color: "#2683FF", 
   },
   {
     title: "E-commerce",
     description:
-      "Criação de sites e aplicações modernas usando React, Tailwind e boas práticas de programação para performance e responsividade.",
-    color: "#2683FF", // neon cyan
+      "Desenvolvimento de lojas virtuais completas, seguras e escaláveis, com foco na experiência do usuário, integração com meios de pagamento e soluções modernas para impulsionar vendas online.",
+    color: "#2683FF", 
   },
   {
     title: "Sistema de Gestão",
     description:
-      "Desenvolvimento e manutenção de sistemas de automação, programação de PLCs e integração de máquinas industriais.",
-    color: "#2683FF", // neon purple
+      "Criação de sistemas de gestão personalizados para controle de processos, dados e operações, com soluções eficientes, seguras e adaptadas às necessidades do negócio.",
+    color: "#2683FF", 
   },
 ];
